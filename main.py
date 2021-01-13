1e12837d3549ce2ff1f4e5bd0bb7b62289cda054
Amartya pandey <amartyapandey19@gmail.com>
2021-01-13 18:33:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+base = Tk()
+base.title("--DIGITAL CLOCK--")
+
+
+
+def digitaltime():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(2000, digitaltime())
+
+
+label = Label(base, font=("ds_digital",70),background = "yellow", foreground = "yellow")
+label.pack(anchor='center')
+
+digitaltime()
+
